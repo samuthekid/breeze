025a60f6c1dbb0f0873c9993dc939aeed853afac
JFilipeG <jfilipenprg@gmail.com>
2018-04-22 09:38:20 +0100

    WIP

--- src/plugins/shortcuts/index.js
+++ src/plugins/shortcuts/index.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 let storage = {
   shortcuts: [
     {
@@ -29,13 +31,42 @@ function getSuggestions(args) {
   }).filter((suggestion) => suggestion != null);
 }
 
+//function saveShortcut(args) {
+//  return {
+//    id: 0,
+//    text: 'Add shortcut',
+//    onEnter: () => addShortcut({
+//      state: {
+//
+//      }
+//    }),
+//  }
+//}
+//
+//function deleteShortcut(args) {
+//  return {
+//    id: 0,
+//    text: 'Remove shortcut',
+//    onEnter: () => removeShortcut(args),
+//  }
+//}
+
 export const shortcuts = {
   label: 'shortcuts',
   cmds: [
     {
       condition: 'beTrue',
-      label: 'shortcuts',
       handler: getSuggestions,
     },
+    //{
+    //  condition: 'startsWith',
+    //  label: 'add',
+    //  handler: saveShortcut,
+    //},
+    //{
+    //  condition: 'startsWith',
+    //  label: 'remove',
+    //  handler: deleteShortcut,
+    //}
   ],
 };
